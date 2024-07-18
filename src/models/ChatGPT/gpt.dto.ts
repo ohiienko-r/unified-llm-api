4abb2225f2bc25b60676ddc7fe38a486e87cad92
Roman Ohiienko <ohiienko.r@gmail.com>
2024-07-18 21:20:50 +0300

    feat: update open ai dependancy, add new models to list

--- src/models/ChatGPT/gpt.dto.ts
+++ src/models/ChatGPT/gpt.dto.ts
@@ -4,6 +4,9 @@ export const GPT_MODEL_NAME = {
   GPT_35_TURBO_16K: "gpt-3.5-turbo-16k",
   GPT_4: "gpt-4",
   GPT_4_TURBO: "gpt-4-turbo",
+  GPT_4O_MINI: "gpt-4o-mini",
+  GPT_4O_MINI_0716: "gpt-4o-mini-2024-07-18",
+  GPT_4O: "gpt-4o",
 } as const;
 
 export const GPT_ROLE = {
